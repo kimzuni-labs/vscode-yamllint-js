aadff9a2b624563c9d94f385970bf18a2d68da8b
kimzuni <me@kimzuni.com>
2026-02-15 00:50:26 +0900

    chore: init with `yo code`

---
+++ README.md
@@ -0,0 +1,11 @@
+# vscode-yamllint-js
+
+VSCode extension for
+[yamllint-js](https://github.com/kimzuni-labs/yamllint-js)
+
+
+
+## License
+
+[GPL version 3](LICENSE)
+(based on [yamllint-js](https://github.com/kimzuni-labs/yamllint-js))
---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig } from "eslint/config";
+import typescriptEslint from "typescript-eslint";
+
+
+
+export default defineConfig([{
+	files: ["**/*.ts"],
+}, {
+	plugins: {
+		"@typescript-eslint": typescriptEslint.plugin,
+	},
+
+	languageOptions: {
+		parser: typescriptEslint.parser,
+		ecmaVersion: 2022,
+		sourceType: "module",
+	},
+
+	rules: {
+		"@typescript-eslint/naming-convention": ["warn", {
+			selector: "import",
+			format: ["camelCase", "PascalCase"],
+		}],
+
+		curly: "warn",
+		eqeqeq: "warn",
+		"no-throw-literal": "warn",
+		semi: "warn",
+	},
+}]);
