b82b974482917bd8ca430ac70771b924247c6062
kimzuni <me@kimzuni.com>
2026-02-20 20:09:24 +0900

    refactor: update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -24,13 +24,14 @@ import { lintDocument } from "./linter";
 export function activate(context: vscode.ExtensionContext) {
 	const collection = vscode.languages.createDiagnosticCollection("yamllint-js");
 
-	const updateDiagnostics = async (e?: vscode.TextEditor | vscode.TextDocumentChangeEvent) => {
-		const document = e?.document;
-		if (document?.languageId === "yaml") {
-			const diagnostics = await lintDocument(document);
-			collection.set(document.uri, diagnostics);
-		} else if (document) {
-			deleteDiagnostics(document);
+	const updateDiagnostics = async (document?: vscode.TextDocument) => {
+		if (document) {
+			if (document.languageId === "yaml") {
+				const diagnostics = await lintDocument(document);
+				collection.set(document.uri, diagnostics);
+			} else {
+				deleteDiagnostics(document);
+			}
 		}
 	};
 
@@ -39,12 +40,12 @@ export function activate(context: vscode.ExtensionContext) {
 	};
 
 	if (vscode.window.activeTextEditor) {
-		updateDiagnostics(vscode.window.activeTextEditor);
+		updateDiagnostics(vscode.window.activeTextEditor.document);
 	}
 
 	context.subscriptions.push(
-		vscode.window.onDidChangeActiveTextEditor(updateDiagnostics),
-		vscode.workspace.onDidChangeTextDocument(updateDiagnostics),
+		vscode.window.onDidChangeActiveTextEditor(e => updateDiagnostics(e?.document)),
+		vscode.workspace.onDidChangeTextDocument(e => updateDiagnostics(e.document)),
 		vscode.workspace.onDidCloseTextDocument(deleteDiagnostics),
 	);
 
